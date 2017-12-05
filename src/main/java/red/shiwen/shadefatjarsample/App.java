e9b8101709d8ca7994d0cf2dad916e1ed44fe41b
shiwen4@leju.com <shiwen4@leju.com>
2017-12-05 08:59:24 +0800

    complete sample

---
+++ src/main/java/red/shiwen/shadefatjarsample/App.java
@@ -0,0 +1,13 @@
+package red.shiwen.shadefatjarsample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
